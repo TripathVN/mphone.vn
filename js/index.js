87891873420c9020c42465c6df455b82ccf83b12
Duy Le <duyleekun@gmail.com>
2018-10-29 17:20:26 +0700

    Add checkout

--- js/index.js
+++ js/index.js
@@ -11,6 +11,8 @@ import $ from 'jquery';
 import 'fullpage.js/vendors/scrolloverflow';
 import FullPage from 'fullpage.js/dist/fullpage';
 
+import md5 from 'md5';
+
 
 import AOS from 'aos';
 
@@ -27,6 +29,7 @@ OM.aos = function () {
         });
     }
 };
+
 
 OM.fullpage = function () {
     new FullPage('#fullpage', {
@@ -80,6 +83,32 @@ $(document).ready(function () {
      * AOS
      */
     OM.aos();
+
+    $('form').submit(() => {
+        const key = {
+            access_code: 'VIDL5nGwd2T4hVSOXsa3cCqxCZOfs7FT',
+            terminal_id: '10',
+            return_url: 'http://mphone.vn/thankyou.html',
+            service_type: 'BP',
+            currency_code: 'VND',
+            amount: '5000000',
+            bill_info: 'MPhone 32GB',
+            bill_no: Date.now()
+        }
+        const stringForMD5 = [
+            key.terminal_id,
+            key.return_url,
+            key.service_type,
+            key.currency_code,
+            key.amount,
+            key.bill_no,
+            key.bill_info,
+            'PPPL5nGwd2T4hVSOXsa3cCqxCZOfs111'
+        ].join("|");
+
+        window.location = `http://118.70.157.239/payment_api/payment?${$.param(Object.assign({check_sum: md5(stringForMD5).toUpperCase()}, key))}`
+        return false
+    })
 });
 
 export default OM;
